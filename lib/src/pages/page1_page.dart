9a75723ab73ec968daa80b89b7b337448a29fa7d
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-29 14:09:19 -0600

    192. SlideTransition

--- lib/src/pages/page1_page.dart
+++ lib/src/pages/page1_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:transition_app/src/pages/pages.dart';
+
 class Page1Page extends StatelessWidget {
   const Page1Page({super.key});
 
@@ -12,10 +14,34 @@ class Page1Page extends StatelessWidget {
       body: const Center(
         child: Text('Page1Page'),
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.access_time),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, _createRoute());
+        },
+        child: const Icon(Icons.access_time),
       ),
+    );
+  }
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (BuildContext context, Animation<double> animation,
+              Animation<double> secondaryAnimation) =>
+          const Page2Page(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final curvedAnimation = CurvedAnimation(
+          parent: animation,
+          curve: Curves.easeInOut,
+        );
+
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(0.5, 1.0),
+            end: Offset.zero,
+          ).animate(curvedAnimation),
+          child: child,
+        );
+      },
     );
   }
 }
